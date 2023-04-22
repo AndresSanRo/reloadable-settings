78f8fd3d84f3890f80506a1e0203f3a1300bc7cb
Andrés Sánchez Robleño <andres.sanche@encamina.com>
2023-04-22 20:15:37 +0200

    feat: add settings service

---
+++ ReloadableSettings.Services/SettingsService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Options;
+using ReloadableSettings.Models;
+
+namespace ReloadableSettings.Services
+{
+    public class SettingsService : ISettingsService
+    {
+        private readonly Settings _settings;
+        public SettingsService(IOptionsSnapshot<Settings> options)
+        {
+            _settings = options.Value;
+        }
+        public string GetHelloWorldSetting()
+        {
+            return _settings.HelloWorld;
+        }
+    }
+}
