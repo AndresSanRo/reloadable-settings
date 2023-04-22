1a2757451d4601514023ff364cb890c70ba534a1
Andrés Sánchez Robleño <andres.sanche@encamina.com>
2023-04-22 19:33:49 +0200

    feat: configure app config

--- ReloadableSettings.Api/Program.cs
+++ ReloadableSettings.Api/Program.cs
@@ -1,20 +1,40 @@
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+string connectionString = builder.Configuration.GetConnectionString("AppConfig");
+
+// Load configuration from Azure App Configuration
+builder.Configuration.AddAzureAppConfiguration(options =>
+{
+    options.Connect(connectionString)
+            .Select(KeyFilter.Any, LabelFilter.Null)
+            .ConfigureRefresh(refreshOptions =>
+            {
+                refreshOptions.Register("Sentinel", refreshAll: true).SetCacheExpiration(TimeSpan.FromSeconds(10));
+            });
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddAzureAppConfiguration();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.EnvironmentName == "Local")
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseAzureAppConfiguration();
 
 app.UseHttpsRedirection();
 
